8454674a85b29cff49641b2a42aca9db0f6bfa9e
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-01-12 21:43:39 +0700

    Function 3 : Tanpa Argument dan Parameter Tetapi Mengembalikan Nilai (Return Value)

---
+++ Algoritma/Eksplorasi Function/function3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void uwu() {
+  asasda;jdajshduwda asjdashdj hawudhajkshd jkahwudh asdh jashdwuaksjd hakjshd uwdaskjdh wukasjdh wkajshdu wkasnn  asjdasd masdh
+}
+
+int main() {
+  cout << "Hello World!";
+}
