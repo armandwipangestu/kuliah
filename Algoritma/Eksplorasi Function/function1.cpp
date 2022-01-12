bafded8c4c78727c6d311e1c3337be0b15867269
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-01-12 21:41:05 +0700

    Function 1 : Tanpa Argument dan Parameter dan Tidak Mengembalikan Nilai (Return Value) atau Procedur

---
+++ Algoritma/Eksplorasi Function/function1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void MenampilkanText() {
+  cout << "Ini adalah text dari function!";
+}
+
+void Penjumlahan() {
+  int x = 10,
+      y = 5,
+      hasil = x + y;
+  
+  cout << hasil;
+}
+
+int main() {
+
+  //MenampilkanText();
+  Penjumlahan();
+
+  return 0;
+}
