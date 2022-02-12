b8add8e0d7bddf81bbd8b6c507e0bde8503bcfc6
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-02-12 12:39:48 +0700

    Kuliah - Pemrograman2 - src - com - pertemuan3 - Luas Lingkaran2

---
+++ Semester (Genap)/Semester 2/Pemrograman 2/src/com/pemrograman2/pertemuan3/LuasLignkaran2.java
@@ -0,0 +1,21 @@
+package com.pemrograman2.pertemuan3;
+
+import java.util.Scanner;
+
+public class LuasLignkaran2 {
+
+    public static void main(String[] args) {
+        float luas_lingkaran_comma, phi_comma, jari_jari_comma;
+        int luas_lingkaran, phi_pembilang, phi_penyebut, jari_jari;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("┌──────────────────────────┐");
+        System.out.println("│  Program Luas Lingkaran  │");
+        System.out.println("└──────────────────────────┘\n");
+
+        System.out.print("Masukan Jari - Jari Lingkaran: ");
+        jari_jari_comma = keyboard.nextFloat();
+    }
+
+}
