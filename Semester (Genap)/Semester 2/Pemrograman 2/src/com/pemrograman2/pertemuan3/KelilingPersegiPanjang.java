5ac02e05f067d3b3a090395a52bd7573fae8ed21
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-02-12 12:07:30 +0700

    Kuliah - Pemrograman2 - src - com - pertemuan3 - Keliling Persegi Panjang

---
+++ Semester (Genap)/Semester 2/Pemrograman 2/src/com/pemrograman2/pertemuan3/KelilingPersegiPanjang.java
@@ -0,0 +1,24 @@
+package com.pemrograman2.pertemuan3;
+
+import java.util.Scanner;
+
+public class KelilingPersegiPanjang {
+    public static void main(String[] args) {
+        int keliling_lingkaran, panjang, lebar;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("┌────────────────────────────────────┐");
+        System.out.println("│  Program Keliling Persegi Panjang  │");
+        System.out.println("└────────────────────────────────────┘\n");
+
+        System.out.print("Masukan Panjang: ");
+        panjang = keyboard.nextInt();
+
+        System.out.print("Masukan Lebar: ");
+        lebar = keyboard.nextInt();
+
+        keliling_lingkaran = 2 * (panjang + lebar);
+        System.out.print("Keliling Lingkaran: " + keliling_lingkaran);
+    }
+}
