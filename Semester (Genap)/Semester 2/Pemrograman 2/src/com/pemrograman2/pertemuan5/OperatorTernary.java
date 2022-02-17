5522025bbfbe320d6513a0da54dd950cb6ec4e75
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-02-17 21:24:05 +0700

    Kuliah - Pemrograman2 - src - com - pertemuan5 - Operator Ternary

---
+++ Semester (Genap)/Semester 2/Pemrograman 2/src/com/pemrograman2/pertemuan5/OperatorTernary.java
@@ -0,0 +1,11 @@
+package com.pemrograman2.pertemuan5;
+
+class OperatorTernary {
+  public static void main(String[] args) {
+    boolean suka = true;
+    String jawaban;
+
+    jawaban = suka ? "iya" : "tidak";
+    System.out.println(jawaban);
+  }
+}
